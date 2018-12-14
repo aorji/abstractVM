0db171ada33b053f80be48ffa87379cabd5387fd
aorji <anastasia.orji@gmail.com>
2018-12-14 18:05:13 +0200

    rm

--- abstractVM/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "IOperand.hpp"
-#include "Float.hpp"
-
-int main(){
-    std::cout << "Hello" << std::endl;
-    Factory f = Factory(int8, 3);
-    std::cout << f->ptrOperand->getType()
-}
